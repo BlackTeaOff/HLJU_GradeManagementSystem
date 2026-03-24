0cb61c4cf1dcb17bd7506295e6d375baaffc8df5
BlackTea <1571106964@qq.com>
2026-03-24 09:16:30 +0800

    first commit

---
+++ src/main/java/com/example/grademanagementsystem/GradeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.grademanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeManagementSystemApplication.class, args);
+    }
+
+}
